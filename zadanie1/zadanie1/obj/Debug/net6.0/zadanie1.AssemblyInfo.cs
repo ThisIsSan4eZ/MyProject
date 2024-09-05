5e3e29cb248385ce770a27ec2f52f05eb6639257
Alex <djstar2015@gmail.com>
2024-09-05 16:14:21 +0300

    Обновление файлов и описания

--- zadanie1/zadanie1/obj/Debug/net6.0/zadanie1.AssemblyInfo.cs
+++ zadanie1/zadanie1/obj/Debug/net6.0/zadanie1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zadanie1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+181dd107503251532ac754aaa49e14ce38ab0967")]
 [assembly: System.Reflection.AssemblyProductAttribute("zadanie1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zadanie1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
